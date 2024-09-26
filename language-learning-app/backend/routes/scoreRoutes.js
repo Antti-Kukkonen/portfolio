b25b62de0a7a66a5bbd9a37bd8e006656a1d1d70
Antti Kukkonen <antti.kukkonen2@gmail.com>
2024-09-26 13:59:51 +0300

    relocated the language learning app

--- language-learning-app/backend/routes/scoreRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-const { postScore } = require('../controllers/scoreController');
-
-router.post('/', protect, postScore);
-
-module.exports = router;
